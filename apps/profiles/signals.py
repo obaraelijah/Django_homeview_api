70e26bff3383ccd5fe111af11c3a74a71f27b1b0
elijah obara <elijahobara357@gmail.com>
2023-06-11 19:54:23 +0300

    chore: Hot fix

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -8,14 +8,13 @@ from real_estate.settings.base import AUTH_USER_MODEL
 
 logger = logging.getLogger(__name__)
 
-
 @receiver(post_save, sender=AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-
 @receiver(post_save, sender=AUTH_USER_MODEL)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+    
     logger.info(f"{instance}'s profile created")
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('supersecret/', admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
